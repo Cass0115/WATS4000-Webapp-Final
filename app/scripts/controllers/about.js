a2118e29eb8715c9936979e0f9de34fd210c899b
Cassie Durr <Cass0115@gmail.com>
2017-07-13 17:56:53 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wats4000WebappFinalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wats4000WebappFinalApp
+ */
+angular.module('wats4000WebappFinalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
