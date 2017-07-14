db3fdcd8252ffe633f9b77511213690d8fd470b8
Cassie Durr <Cass0115@gmail.com>
2017-07-13 18:05:32 -0700

    update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:Cass0115/WATS4000-Webapp-Final.git',
           branch: 'gh-pages'
         }
       }
